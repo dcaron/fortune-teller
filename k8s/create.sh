fda5456bae842d2046da9bb4a4d9e8ed03843ca9
David Caron <dcaron@xs4all.nl>
2018-10-17 17:50:12 +0200

    fix typos

--- k8s/create.sh
+++ k8s/create.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 kubectl apply -f conf-database.yml
-kubectl apply -f conf-secrets.yaml
+kubectl apply -f conf-secrets.yml
 
 kubectl apply -f fortune-service-k8s-database.yml
 kubectl apply -f fortune-service-k8s-deployment-with-database-v2.yml
 kubectl apply -f fortune-service-k8s-service.yml
 
-kubectl apply -f conf-ui.yaml
+kubectl apply -f conf-ui.yml
 
 kubectl apply -f fortune-teller-ui-k8s-deployment-v1.yml
 kubectl apply -f fortune-teller-ui-k8s-service.yml
